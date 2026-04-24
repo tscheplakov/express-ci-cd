67c11f4308fb18ec9f2defdd9b8dbbd113bcd25f
Lev <lipptonderry@gmail.com>
2026-04-25 00:01:56 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+﻿const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express! Version 1.0');
+});
+
+app.listen(PORT, () => {
+  console.log(Server running on port  + PORT);
+});
